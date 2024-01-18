85c90b4f19621074e1c570e8bd3458c888b8bb81
Vaibhav Chawla <recksoldier@Vaibhavs-MacBook-Air.local>
2024-01-18 21:30:45 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const cors = require("cors");
 
 // express app
 const app = express()
 
 // middleware
-app.use(express.json())
+const corsOptions = {
+  origin: "http://localhost:4000/api/workouts" // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
